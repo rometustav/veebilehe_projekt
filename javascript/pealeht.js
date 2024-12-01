850709946f369877c71b3c51c32c4deef545725c
markusmuts <97757358+markusmuts@users.noreply.github.com>
2024-12-01 19:17:43 +0200

    pealehe js kommenteeritud

--- javascript/pealeht.js
+++ javascript/pealeht.js
@@ -3,12 +3,15 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting){
+            // Asendab kõik "boxes-hidden", mis on näha, ära "boxes-showing"
             entry.target.classList.add('boxes-showing');
         } else {
+            // Muudab jälle selle kasti peidetuks
             entry.target.classList.remove('boxes-showing');
         }
     });
 });
 
+// Valib kõik HTML-i elemendid, mille klass on "boxes-hidden"
 const hiddenElements = document.querySelectorAll('.boxes-hidden');
 hiddenElements.forEach((el) => observer.observe(el))
